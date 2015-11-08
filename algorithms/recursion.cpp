9a3e8e230056e544683f8385757b45335e9d0605
brucetting <ych_ding@163.com>
2015-11-08 21:34:26 +0800

    add recursion.cpp to solve N queens problems.

---
+++ algorithms/recursion.cpp
@@ -0,0 +1,161 @@
+/*
+ * recursion.cpp
+ * demostrate recursion algorithms to solve some classic problems, such as
+ * 8-queens.
+ *
+ * Source File
+ *
+ * Copyright (C) 2014-2015  Yaochuang Ding - <ych_ding@163.com>
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to 
+ * deal in the Software without restriction, including without limitation the 
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, 
+ *    this list of conditions, and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice, 
+ *    this list of conditions and the following disclaimer in the documentation 
+ *    and/or other materials provided with the distribution, and in the same 
+ *    place and form as other copyright, license and disclaimer information.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL 
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN 
+ * THE SOFTWARE.
+ *
+ */
+ 
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <cassert>
+#include <map>
+#include <vector>
+#include <iostream>
+#include "Logger.h"
+
+using namespace std;
+
+#define PERFORMANCE_METER
+
+/*************************************************
+ *  The class is to solver N Queens problem.
+ *
+ *  NOTE: max N supported is 32.
+*************************************************/
+class NQueensSolver 
+{
+private:
+        map<int, int> columnMap;
+private:
+    bool checkPath(vector<int>& path)
+    {
+        int curRow = path.size() -1;
+        int num2   = path[curRow];
+        for(int i = 0; i < curRow; i++)
+        {
+            int num1    = path[i];
+            int deltRow = curRow - i;
+            if(num2 & num1 || num2 & (num1 << deltRow) || num2 & (num1 >> deltRow)) return false;
+        }
+        return true;
+    }
+    vector<string> path2Board( vector<int>& curPath)
+    {
+        int n = curPath.size();
+        vector<string> ret(n, string(n, '.')); /* n == 0 is ok */
+        for(int i = 0; i < n; ++i)
+        {
+            int col = columnMap[curPath[i]];  
+            ret[i][col] = 'Q';
+        }
+        return ret;
+    }
+    void solveNQueensHelper(vector<vector<string> >& allPath, vector<int>& curPath, int n, int curRow)
+    {
+        /* stop search condition */
+        if(curRow == n) { allPath.push_back(path2Board(curPath)); return;   }
+        
+        /* search column: each row search from 0 ==> n-1 */
+        for(int j = 0; j < n; j++)
+        {
+            int curNum = 1 << j;
+            curPath.push_back(curNum); /* test current column position */
+            
+            /* current path ok, search for next row */
+            if(true == checkPath(curPath))  
+            { solveNQueensHelper(allPath, curPath, n, curRow + 1); }
+            
+            curPath.pop_back(); /* remove current column positon, test next column position */
+        }
+    }
+    
+public:  
+    
+    /*************************************************
+     *  The public interface is to solver N Queens 
+     *  problem.
+     *
+     *  it returns all possible chessboard.
+     *  each chessboard is describled by a vector<string>
+     *  while the queen is represented by char 'Q'
+     *************************************************/
+    vector<vector<string> > solveNQueens(int n) 
+    {
+         #ifdef PERFORMANCE_METER
+        clock_t clk1, clk2;
+        clk1 = clock(); /* get current clcok ticks elapsed since epoch */
+        #endif
+    
+        vector<vector<string> > allPath;
+        vector<int> curPath;
+        for(int i = 0; i < n; ++i)
+        { 
+            columnMap[1 << i] = n-1-i;
+        }
+        solveNQueensHelper(allPath, curPath, n, 0);
+        
+        #ifdef PERFORMANCE_METER
+        clk2 = clock();
+        float seconds = ((float)(clk2 - clk1)) / CLOCKS_PER_SEC; /* calculate in seconds units */
+        printf("[Solve time] = %ld ticks, %.4f seconds!\t Solve %d Queen!\n", clk2 - clk1, seconds, n);
+        #endif
+        return allPath;
+    }
+};
+
+/*************************************************
+ * just call the demo string.
+*************************************************/
+int main(int argc, char** argv)
+{
+  logger.setLevel(DEBUG);
+  logger.setLineLevel(DEBUG);
+  ENTER_FUNCTION;
+  
+  int n;
+  NQueensSolver solver;
+  while (true)
+  {
+    cout << "Enter Queens number:";
+    cin >> n;
+    if (-1 == n)
+    {
+        cout << "Game Over." << std::endl;
+        return 0;
+    }
+    solver.solveNQueens(n);
+  }
+  
+  EXIT_FUNCTION;
+  return 0;
+}
+
