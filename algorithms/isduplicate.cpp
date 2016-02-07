9c99225ca7cc048e42bec432deb8387bfc42d213
brucetting <ych_ding@163.com>
2016-02-07 12:15:32 +0800

    add license

--- algorithms/isduplicate.cpp
+++ algorithms/isduplicate.cpp
@@ -1,3 +1,31 @@
+/*
+ * Copyright (C) 2014-2015  Yaochuang Ding - <ych_ding@163.com>
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to 
+ * deal in the Software without restriction, including without limitation the 
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, 
+ *    this list of conditions, and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice, 
+ *    this list of conditions and the following disclaimer in the documentation 
+ *    and/or other materials provided with the distribution, and in the same 
+ *    place and form as other copyright, license and disclaimer information.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL 
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN 
+ * THE SOFTWARE.
+ *
+ */
+
 #include <cstdio>
 #include <cstdlib>
 #include <cstring>
@@ -8,25 +36,33 @@ using namespace std;
 
 typedef enum tagLogLevel
 {
-LOG_LEVEL_ERR,
-LOG_LEVEL_INFO,
-LOG_LEVEL_DBG,
-LOG_LEVEL_ALL,
+	LOG_LEVEL_ERR,
+	LOG_LEVEL_INFO,
+	LOG_LEVEL_DBG,
+	LOG_LEVEL_ALL,
 } LogLevel;
 
 LogLevel gCurLoglevel = LOG_LEVEL_ERR;
 
-#define LOG_D(fmt, ...)  do { if (gCurLoglevel >= LOG_LEVEL_DBG) fprintf(stdout,"[ DEBUG ] "fmt,##__VA_ARGS__ ); } while(0)
-#define LOG_E(fmt, ...)  do { if (gCurLoglevel >= LOG_LEVEL_ERR) fprintf(stdout,"[ ERROR ] "fmt,##__VA_ARGS__ ); } while(0)
-#define LOG_I(fmt, ...)  do { if (gCurLoglevel >= LOG_LEVEL_INFO) fprintf(stdout,"[ INFO ] "fmt,##__VA_ARGS__ ); } while(0)
+//#define LOG_D(fmt, ...)  do { if (gCurLoglevel >= LOG_LEVEL_DBG) fprintf(stdout,"[ DEBUG ] "fmt,##__VA_ARGS__ ); } while(0)
+//#define LOG_E(fmt, ...)  do { if (gCurLoglevel >= LOG_LEVEL_ERR) fprintf(stdout,"[ ERROR ] "fmt,##__VA_ARGS__ ); } while(0)
+//#define LOG_I(fmt, ...)  do { if (gCurLoglevel >= LOG_LEVEL_INFO) fprintf(stdout,"[ INFO ] "fmt,##__VA_ARGS__ ); } while(0)
 
-/* 
+/*************************************************
+ * Function: 
+ *   
+ * Param[in]:   
+ * Param[in]:   
+ *   
+ * Retrun: 
+ *   
+ * Notice:   
  * read data from stdin and check the input sequenc duplicated or not.
  * take advantage of C++ STL set.insert() to check the duplicate
  * reference location:
  * http://www.cplusplus.com/reference/set/set/insert/
- */
-int check_duplicate()
+*************************************************/
+int isDuplicate()
 {
 	set<int> s;
 	pair<set<int>::iterator, bool> ret;
@@ -44,9 +80,19 @@ int check_duplicate()
 	return 0;
 }
 
+/*************************************************
+ * Function: 
+ *   
+ * Param[in]:   
+ * Param[out]:   
+ *   
+ * Retrun: 
+ *   
+ * Notice:   
+*************************************************/
 int main(int argc, char** argv)
 {  
-  int ret = check_duplicate();
+  int ret = isDuplicate();
   printf("duplicate: %d\n", ret);
   return 0;
 }
