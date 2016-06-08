b29b1d64ed7c4d96d3fee8b7a77e077451ddc50c
ychding11 <ych_ding@163.com>
2016-06-08 08:10:32 +0800

    1. add a macro to print std container elements.

--- algorithms/tester.cpp
+++ algorithms/tester.cpp
@@ -1,8 +1,27 @@
 #include "Logger.h"
+#include "utils.h"
+#include<set>
 
-int main(void)
+using std::set;
+using namespace test;
+
+#define printContainer(elements)   \
+{   \ 
+    std::cout << "- " << #elements << " Elements:" << std::endl;   \
+    for (auto e : elements) std::cout << e << " ";  \
+    std::cout << std::endl;   \
+    std::cout << "- End Elements" << std::endl;   \
+}
+
+int main()
 {
 	Logger log("./log.txt");
-	log << "This is a test!" << std::endl;
-	return 0;
+	log << "Test log!" << std::endl;
+
+    vector<int> nums = {1, 2, 3};
+    vector<int> sums = allSubsetSum(nums);
+    printContainer(sums);
+    set<int> sets;
+    for (auto a : sums) sets.insert(a);
+    printContainer(sets);
 }
