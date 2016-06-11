d5e418146c11beb959e8b223a60e6f25310f71ea
ychding11 <ych_ding@163.com>
2016-06-11 10:01:42 +0800

    1. modify macro. 2. add test case.

--- algorithms/test/subsetTester.cpp
+++ algorithms/test/subsetTester.cpp
@@ -6,34 +6,61 @@ using std::set;
 using namespace test;
 
 #define printContainer(elements)   \
-do {   \ 
-    std::cout << "- " << #elements << " Elements:" << std::endl;   \
+do {   \
+    std::cout << "- " << #elements << " Elements: ";   \
     for (auto e : elements) std::cout << e << " ";  \
     std::cout << std::endl;   \
-    std::cout << "- End Elements" << std::endl;   \
 } while(0)
 
 int main()
 {
-    vector<int> nums = {1, 2, 3};
+    DataSource datasource;
+    vector<int> nums(4);
+    datasource.fillInData(nums, 100);
+    printContainer(nums);
     vector<int> sums = allSubsetSum(nums);
     printContainer(sums);
     set<int> sets;
     for (auto a : sums) sets.insert(a);
     printContainer(sets);
+    vector<int> exludeDataset = datasource.dataSetExlude(sets, 100);
+    printContainer(exludeDataset);
     int fail = 0, success = 0; 
-    for (auto a : sets)
+    for (auto a : exludeDataset)
     {
-        if (0)
+        if (subsetExist(nums, a))
         {
-            ++success;
+            ++fail;
+            std::cout << "- [Test Failed]";
+            std::cout << "  Exlude dataset Test." << std::endl;
+            std::cout << "- -------------" << std::endl;
+            printContainer(nums);
+            std::cout << "- Target sum: " << a << std::endl;
+            std::cout << "- -------------" << std::endl;
         }
         else
         {
-            ++fail;
+            ++success;
         }
     }
-    std::cout << "Test result  : "
+    for (auto a : sums)
+    {
+        if (!subsetExist(nums, a))
+        {
+            ++fail;
+            std::cout << "- [Test Failed]";
+            std::cout << "  Include dataset Test." << std::endl;
+            std::cout << "- -------------" << std::endl;
+            printContainer(nums);
+            std::cout << "- Target sum: " << a << std::endl;
+            std::cout << "- -------------" << std::endl;
+        }
+        else
+        {
+            ++success;
+        }
+    }
+    std::cout << "- Test result  : "
               << success << " cases success,"
               << fail << " cases fail."
               << std::endl;
