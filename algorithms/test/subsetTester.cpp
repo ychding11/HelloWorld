db4a5daf61d3ea8a35e167d14ad51d6b392944f4
ychding11 <ych_ding@163.com>
2016-06-11 13:46:28 +0800

    1. add macro to print Test case infomation. 2. add a new test function for min-subset-sum-difference.

--- algorithms/test/subsetTester.cpp
+++ algorithms/test/subsetTester.cpp
@@ -1,8 +1,10 @@
 #include "utils.h"
 #include "algorithms.h"
 #include<set>
+#include<limits>
 
 using std::set;
+using std::numeric_limits;
 using namespace test;
 
 #define printContainer(elements)   \
@@ -12,6 +14,67 @@ do {   \
     std::cout << std::endl;   \
 } while(0)
 
+#define printTestName(name) \
+{ \
+    std::cout << "- Test " << #name << " Begins." << std::endl; \
+}
+
+#define printTestResult(success, fail) \
+{ \
+    std::cout << "- Test result : "    \
+              << success << " cases success," \
+              << fail << " cases fail." \
+              << std::endl; \
+}
+
+void testMinSubsetDifference(int maxElementValue = 10, int maxElementNum = 3, int testcases = 3)
+{
+    int fail = 0, success = 0; 
+    DataSource datasource;
+    while (--testcases >= 0) 
+    {
+    vector<int> nums(maxElementNum);
+    datasource.fillInData(nums, maxElementValue);
+    vector<int> sums = allSubsetSum(nums);
+    set<int> sets;
+    int sum = 0; 
+    int minDiff = numeric_limits<int>::max();
+    for (auto a : nums)
+    {
+        sum += a;
+    }
+    for (auto a : sums)
+    {
+        sets.insert(a);
+    }
+    
+    for (auto a : sets)
+    {
+        if (abs(sum - a - a) < minDiff)
+            minDiff = abs(sum - a - a);
+    }
+    printContainer(nums);
+    printContainer(sums);
+    printContainer(sets);
+    printTestName(minSubsetDifference);
+    if (minDiff != minSubsetDifference(nums))
+    {
+            ++fail;
+            std::cout << "- [Test Failed]";
+            std::cout << "  Min Subset sum different Test." << std::endl;
+            std::cout << "- -------------" << std::endl;
+            printContainer(nums);
+            std::cout << "- Target difference: " << minDiff << std::endl;
+            std::cout << "- -------------" << std::endl;
+    }
+    else
+    {
+            ++success;
+    }
+    printTestResult(success, fail);
+    }
+}
+
 int main(int argc, char* argv[])
 {
     int maxElementValue = 10;
@@ -20,6 +83,8 @@ int main(int argc, char* argv[])
         maxElementNum = atoi(argv[1]);
     if (argc == 3)
         maxElementValue = atoi(argv[2]);
+
+    testMinSubsetDifference();
 
     DataSource datasource;
     vector<int> nums(maxElementNum);
@@ -33,6 +98,7 @@ int main(int argc, char* argv[])
     vector<int> exludeDataset = datasource.dataSetExlude(sets, maxElementValue);
     printContainer(exludeDataset);
     
+    printTestName(subsetExist);
     // Prepare test dataset ok, begin to test.
     int fail = 0, success = 0; 
     for (auto a : exludeDataset)
@@ -69,8 +135,5 @@ int main(int argc, char* argv[])
             ++success;
         }
     }
-    std::cout << "- Test result  : "
-              << success << " cases success,"
-              << fail << " cases fail."
-              << std::endl;
+    printTestResult(success, fail);
 }
