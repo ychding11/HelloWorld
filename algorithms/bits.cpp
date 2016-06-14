3eaf3145f9cec6bad6842dd1b607feaa98fb14d9
ychding11 <ych_ding@163.com>
2016-06-15 07:49:00 +0800

    1. add a file bits.cpp to include some interesting bit operations.

---
+++ algorithms/bits.cpp
@@ -0,0 +1,38 @@
+#include <cstdlib>
+#include <cassert>
+#include <iostream>
+
+#if !defined(BUILD_LIB)
+#include<bitset>
+using std::bitset;
+#endif
+
+
+bool isPowerOf4(unsigned int n)
+{
+    if (n > 0 && !(n & (n - 1)))
+    {
+        for (unsigned int i = 0; i < sizeof(n) * 8; i += 2)
+        {
+            if ((n >> i) & 0x1) return true;
+        }
+    }
+    return false;
+}
+
+/*! \brief return min by bit operation.
+ * http://graphics.stanford.edu/~seander/bithacks.html#IntegerMinOrMax 
+ * -1 = ~0; b ^ a ^ b = a; a ^ a = 0; a ^ 0 = a;
+ */
+bool minByBit(int a, int b)
+{
+    return b ^ ((a ^ b) & -(a < b));
+}
+
+#if !defined(BUILD_LIB)
+
+int main()
+{
+    return 0;
+}
+#endif
