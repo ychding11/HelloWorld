8bb379b90fbf02e4b13f2015c0f5780a17b0b394
ychding11 <ych_ding@163.com>
2016-06-16 22:39:26 +0800

    1. add algorithms xor find only appear one time elements.

--- algorithms/bits.cpp
+++ algorithms/bits.cpp
@@ -1,6 +1,7 @@
 #include <cstdlib>
 #include <cassert>
 #include <iostream>
+#include <vector>
 #include "bits.h"
 
 #if !defined(BUILD_LIB)
@@ -8,10 +9,31 @@
 using std::bitset;
 #endif
 
-/*! \brief how to only keep right most bit */
+using std::vector;
+
+/*! \brief an array of number, all number occur twice except one, find that number. */
+int unpairNumber(const vector<int> &nums)
+{
+    int ret = 0;
+    for (auto a : nums) ret ^= a;
+    return ret;
+}
+
+/*! \brief an array of number, all number occur twice except two, find them. */
+void unpairNumbers(const vector<int> &nums, int &x, int &y)
+{
+    int ret = 0;
+    for (auto a : nums) ret ^= a;
+    ret = ret & (1 << firstSettedBit(ret)); 
+    x = y = 0;
+    for (auto a : nums)
+    {
+        if (ret & a) x ^= a;
+        else y ^= a;
+    }
+}
 
 #if !defined(BUILD_LIB)
-
 int main()
 {
     return 0;
