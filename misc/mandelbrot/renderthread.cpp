4cdee13c9e1e2103169db55049ec6d67fd5f539b
ychding11 <ych_ding@163.com>
2016-07-28 23:58:05 +0800

    1. modify style

--- misc/mandelbrot/renderthread.cpp
+++ misc/mandelbrot/renderthread.cpp
@@ -1,54 +1,14 @@
-/****************************************************************************
-**
-** Copyright (C) 2015 The Qt Company Ltd.
-** Contact: http://www.qt.io/licensing/
-**
-** This file is part of the examples of the Qt Toolkit.
-**
-** $QT_BEGIN_LICENSE:BSD$
-** You may use this file under the terms of the BSD license as follows:
-**
-** "Redistribution and use in source and binary forms, with or without
-** modification, are permitted provided that the following conditions are
-** met:
-**   * Redistributions of source code must retain the above copyright
-**     notice, this list of conditions and the following disclaimer.
-**   * Redistributions in binary form must reproduce the above copyright
-**     notice, this list of conditions and the following disclaimer in
-**     the documentation and/or other materials provided with the
-**     distribution.
-**   * Neither the name of The Qt Company Ltd nor the names of its
-**     contributors may be used to endorse or promote products derived
-**     from this software without specific prior written permission.
-**
-**
-** THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-** "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-** LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-** A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
-** OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-** SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-** LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-** DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-** THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-** (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-** OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE."
-**
-** $QT_END_LICENSE$
-**
-****************************************************************************/
 
 #include "renderthread.h"
-
 #include <QtWidgets>
 #include <cmath>
 
+// Constructor
 RenderThread::RenderThread(QObject *parent)
     : QThread(parent)
+    , restart(false)
+    , abort(false)
 {
-    restart = false;
-    abort = false;
-
     for (int i = 0; i < ColormapSize; ++i)
         colormap[i] = rgbFromWaveLength(380.0 + (i * 400.0 / ColormapSize));
 }
@@ -59,7 +19,6 @@ RenderThread::~RenderThread()
     abort = true;
     condition.wakeOne();
     mutex.unlock();
-
     wait();
 }
 
@@ -73,9 +32,12 @@ void RenderThread::render(double centerX, double centerY, double scaleFactor,
     this->scaleFactor = scaleFactor;
     this->resultSize = resultSize;
 
-    if (!isRunning()) {
+    if (!isRunning())
+    {
         start(LowPriority);
-    } else {
+    }
+    else
+    {
         restart = true;
         condition.wakeOne();
     }
@@ -83,7 +45,8 @@ void RenderThread::render(double centerX, double centerY, double scaleFactor,
 
 void RenderThread::run()
 {
-    forever {
+    forever
+    {
         mutex.lock();
         QSize resultSize = this->resultSize;
         double scaleFactor = this->scaleFactor;
@@ -97,22 +60,22 @@ void RenderThread::run()
 
         const int NumPasses = 8;
         int pass = 0;
-        while (pass < NumPasses) {
+        while (pass < NumPasses)
+        {
             const int MaxIterations = (1 << (2 * pass + 6)) + 32;
             const int Limit = 4;
             bool allBlack = true;
 
-            for (int y = -halfHeight; y < halfHeight; ++y) {
-                if (restart)
-                    break;
-                if (abort)
-                    return;
+            for (int y = -halfHeight; y < halfHeight; ++y)
+            {
+                if (restart) break;
+                if (abort) return;
 
-                uint *scanLine =
-                        reinterpret_cast<uint *>(image.scanLine(y + halfHeight));
+                uint *scanLine = reinterpret_cast<uint *>(image.scanLine(y + halfHeight));
                 double ay = centerY + (y * scaleFactor);
 
-                for (int x = -halfWidth; x < halfWidth; ++x) {
+                for (int x = -halfWidth; x < halfWidth; ++x)
+                {
                     double ax = centerX + (x * scaleFactor);
                     double a1 = ax;
                     double b1 = ay;
@@ -122,37 +85,39 @@ void RenderThread::run()
                         ++numIterations;
                         double a2 = (a1 * a1) - (b1 * b1) + ax;
                         double b2 = (2 * a1 * b1) + ay;
-                        if ((a2 * a2) + (b2 * b2) > Limit)
-                            break;
+                        if ((a2 * a2) + (b2 * b2) > Limit) break;
 
                         ++numIterations;
                         a1 = (a2 * a2) - (b2 * b2) + ax;
                         b1 = (2 * a2 * b2) + ay;
-                        if ((a1 * a1) + (b1 * b1) > Limit)
-                            break;
+                        if ((a1 * a1) + (b1 * b1) > Limit) break;
                     } while (numIterations < MaxIterations);
 
-                    if (numIterations < MaxIterations) {
+                    if (numIterations < MaxIterations)
+                    {
                         *scanLine++ = colormap[numIterations % ColormapSize];
                         allBlack = false;
-                    } else {
+                    }
+                    else
+                    {
                         *scanLine++ = qRgb(0, 0, 0);
                     }
                 }
             }
 
-            if (allBlack && pass == 0) {
+            if (allBlack && pass == 0)
+            {
                 pass = 4;
-            } else {
-                if (!restart)
-                    emit renderedImage(image, scaleFactor);
+            }
+            else
+            {
+                if (!restart) emit renderedImage(image, scaleFactor);
                 ++pass;
             }
         }
 
         mutex.lock();
-        if (!restart)
-            condition.wait(&mutex);
+        if (!restart) condition.wait(&mutex);
         restart = false;
         mutex.unlock();
     }
