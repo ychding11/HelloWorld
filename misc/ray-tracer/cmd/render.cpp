7b2edb5a6962264beab1cbe6045dae11bf675152
ding <ding@eis64gj422.ads.autodesk.com>
2016-08-26 14:37:32 +0800

    1. modify render usage.

--- misc/ray-tracer/cmd/render.cpp
+++ misc/ray-tracer/cmd/render.cpp
@@ -1,5 +1,3 @@
-#include "Pinhole.h"
-#include "FishEye.h"
 #include "Camera.h"
 #include "World.h"
 
@@ -7,9 +5,6 @@ int main()
 {
     World world;
     world.build();
-    world.render_scene();
-    //Pinhole camera;
-    //FishEye camera;
-    //Camera *camera_ptr = world.get_camera();
-    //camera_ptr->render_scene(world);
+    //world.render_scene();
+    world.render_orthographic();
 }
