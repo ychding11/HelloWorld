589361c891f86e7d6dffaa86799b17a117191c15
ychding11 <ych_ding@163.com>
2016-08-31 22:02:25 +0800

    1. add test camera

--- misc/ray-tracer/cmd/render.cpp
+++ misc/ray-tracer/cmd/render.cpp
@@ -3,6 +3,23 @@
 #include <cstdlib>
 
 
+void testCamera(int num)
+{
+    World world;
+    world.build();
+    world.set_pixel_samples(num);
+    world.render_scene();
+}
+
+void testAA(int num)
+{
+    AAScene scene;
+    World &world = scene; 
+    world.build();
+    world.set_pixel_samples(num);
+    world.render_orthographic();
+}
+
 int main(int argc, char **argv)
 {
     int samples = 4;
@@ -12,10 +29,6 @@ int main(int argc, char **argv)
         if (temp > 0) samples = temp;
     }
 
-    AAScene scene;
-    World &world = scene; 
-    world.build();
-    world.set_pixel_samples(samples);
-    world.render_scene();
-    //world.render_orthographic();
+    //testAA(samples);
+    testCamera(samples);
 }
