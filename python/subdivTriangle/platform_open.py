875fe493640e0bc5d86ee60ae1035127bda2914d
ychding11 <ych_ding@163.com>
2016-09-20 08:26:24 +0800

    1. add a python subdiv demo for triangle

---
+++ python/subdivTriangle/platform_open.py
@@ -0,0 +1,13 @@
+﻿from platform import uname as platform_name
+import os
+
+def platform_open(str):
+    """Open specified file using your window manager's preferred application"""
+    name = platform_name()[0]
+    if name == "Linux":
+        print "- On Linux Platform prepare opening ctmviewer."
+        os.system("ctmviewer  " + str)
+    elif name == "Windows":
+        os.system("start " + str)
+    else:
+        os.system("open " + str)
---
+++ python/subdivTriangle/subdiv_sphere.py
@@ -0,0 +1,31 @@
+﻿#!/usr/bin/python
+
+import sys
+from ctypes import *
+import polyhedra, blob
+
+# Import OpenCTM python bindings
+from openctm import *
+from platform_open import *
+
+###############################
+
+filename = "sphere.ctm"
+num_subdivisions = int(sys.argv[1])
+print "- Subdiv level: ", num_subdivisions
+
+verts, faces = polyhedra.icosahedron()
+for x in xrange(num_subdivisions):
+    verts, faces = polyhedra.subdivide(verts, faces)
+
+pVerts = blob.make_blob(verts, c_float)
+pFaces = blob.make_blob(faces, c_uint)
+pNormals = POINTER(c_float)()
+
+ctm = ctmNewContext(CTM_EXPORT)
+ctmDefineMesh(ctm, pVerts, len(verts), pFaces, len(faces), pNormals)
+ctmSave(ctm, filename)
+ctmFreeContext(ctm)
+
+# Open ctm file
+platform_open(filename)
