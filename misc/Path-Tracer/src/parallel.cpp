16bc79b2c5ddfb54cc4b8ed89e8de02963256729
Yaochuang Ding <ych_ding@163.com>
2017-05-24 03:23:28 +0800

    add multi-thread functionality.

---
+++ misc/Path-Tracer/src/parallel.cpp
@@ -0,0 +1,150 @@
+#include "parallel.h"
+#include <thread>
+#include <vector>
+#include <mutex>
+#include <iostream>
+#include <cstdio>
+#include <condition_variable>
+#include <functional>
+
+
+class ParallelForLoop; // forward declare.
+static std::vector<std::thread> threads;
+static std::mutex workListMutex;
+static bool shutdowThread = false;
+static std::condition_variable workListCondition;
+static ParallelForLoop *workList = nullptr;
+
+thread_local int threadIndex;
+
+int numSystemCores(void)
+{
+    return std::max(1u, std::thread::hardware_concurrency());
+}
+
+int maxThreadIndex(void)
+{
+    return numSystemCores();
+}
+
+class ParallelForLoop
+{
+public:
+    ParallelForLoop(std::function<void(int64_t)> func, int64_t maxIndex, int chunkSize)
+        :mFunc(func)
+        ,mMaxIndex(maxIndex)
+        ,mChunkSize(chunkSize) { }
+
+public:
+
+    const int64_t mMaxIndex;
+    const int mChunkSize;
+          int64_t mNextIndex = 0;
+          int mActiveWorkers = 0;
+    std::function<void(int64_t)> mFunc;
+    ParallelForLoop *next = nullptr;
+
+public:
+    bool Finished(void) const
+    {
+        return mNextIndex >= mMaxIndex && mActiveWorkers == 0;
+    }
+
+};
+
+void workerThread(int tIndex)
+{
+    printf("- Thread %2d beginning.\n", tIndex);
+    threadIndex = tIndex;
+    
+    std::unique_lock<std::mutex> lock(workListMutex);
+    while (!shutdowThread)
+    {
+       if (!workList) 
+       {
+           workListCondition.wait(lock);
+       }
+       else
+       {
+            // Thread wakes and does job here.
+            ParallelForLoop &loop = *workList;
+            int64_t indexStart = loop.mNextIndex;
+            int64_t indexEnd   = std::min(indexStart + loop.mChunkSize, loop.mMaxIndex);
+            loop.mNextIndex = indexEnd;
+            if (loop.mNextIndex == loop.mMaxIndex) workList = loop.next;
+            loop.mActiveWorkers++;
+
+            lock.unlock();
+            
+            for (uint64_t i = indexStart; i < indexEnd; ++i)
+            {
+                if (loop.mFunc) loop.mFunc(i);
+            }
+            lock.lock();
+            loop.mActiveWorkers--;
+            if (loop.Finished()) workListCondition.notify_all();
+       }
+    }
+    printf("- Thread %2d exiting.\n", tIndex);
+}
+
+void ParallelFor(std::function<void(int64_t)> func, int64_t count, int chunkSize)
+{
+   if (threads.size() == 0 || count < chunkSize) 
+   {
+       for (int64_t i = 0; i < count; ++i) func(i);
+       return;
+   }
+
+   ParallelForLoop loop(std::move(func), count, chunkSize);
+   workListMutex.lock();
+   loop.next = workList;
+   workList = &loop;
+   workListMutex.unlock();
+
+   std::unique_lock<std::mutex> lock(workListMutex);
+   workListCondition.notify_all();
+
+   while (!loop.Finished())
+   {
+        int64_t indexStart = loop.mNextIndex;
+        int64_t indexEnd   = std::min(indexStart + loop.mChunkSize, loop.mMaxIndex);
+        loop.mNextIndex = indexEnd;
+        if (loop.mNextIndex == loop.mMaxIndex) workList = loop.next;
+        loop.mActiveWorkers++;
+
+        lock.unlock();
+        
+        for (uint64_t i = indexStart; i < indexEnd; ++i)
+        {
+            if (loop.mFunc) loop.mFunc(i);
+        }
+        lock.lock();
+        loop.mActiveWorkers--;
+   }
+}
+
+void parallelInit(void)
+{
+    if (threads.size() > 0) return;
+    int nThreads = maxThreadIndex();
+    threadIndex = 0;
+    for (int i = 0; i < nThreads - 1; ++i)
+        threads.push_back(std::thread(workerThread, i+1));
+}
+
+void parallelCleanup(void)
+{
+    if (threads.size() == 0) return;
+
+    {
+        std::lock_guard<std::mutex> lock(workListMutex);
+        shutdowThread = true;
+        workListCondition.notify_all();
+    }
+
+    for (std::thread &thread : threads) thread.join();
+    threads.erase(threads.begin(), threads.end());
+    shutdowThread = false;
+}
+
--- misc/Path-Tracer/src/renderer.cpp
+++ misc/Path-Tracer/src/renderer.cpp
@@ -1,9 +1,13 @@
+
 #include <vector>
 #include <stdio.h>
 #include <iostream>
 
 #include "renderer.h"
 #include "../lib/lodepng/lodepng.h"
+#include "parallel.h"
+
+#define MULTI_THREAD
 
 // Clamp double to min/max of 0/1
 inline double clamp(double x){ return x<0 ? 0 : x>1 ? 1 : x; }
@@ -16,9 +20,9 @@ Renderer::Renderer(Scene *scene, Camera *camera)
     try
     {
         m_pixel_buffer = new Vec[m_camera->get_width()*m_camera->get_height()];
-    } catch(...)
+    }
+    catch(...)
     {
-        // no need to delete.
         throw;
     }
 }
@@ -27,28 +31,45 @@ void Renderer::render(int samples)
 {
     int width  = m_camera->get_width();
     int height = m_camera->get_height();
-    double samples_recp = 1./samples;
+    double invSamples = 1./samples;
 
+#ifndef MULTI_THREAD
     // Main Loop
     #pragma omp parallel for schedule(dynamic, 1)       // OpenMP
     for (int y = 0; y < height; y++)
     {
         unsigned short Xi[3] = {0, 0, y*y*y};   // Stores seed for erand48
 
-        fprintf(stderr, "\rRendering (%i samples): %.2f%% ",      // Prints
-                samples, (double)y/height*100);                   // progress
+        fprintf(stderr, "\rRendering (%i samples): %.2f%% ",  samples, (double)y/height*100);  // report progress
 
         for (int x = 0; x < width; x++)
         {
-            Vec col = Vec();
+            Vec color = Vec();
             for (int a = 0; a < samples; a++)
             {
                 Ray ray = m_camera->get_ray(x, y, a > 0, Xi);
-                col = col + m_scene->trace_ray(ray,0,Xi);
+                color = color + m_scene->trace_ray(ray,0,Xi);
             }
-            m_pixel_buffer[(y)*width + x] = col * samples_recp;
+            m_pixel_buffer[(y)*width + x] = color * invSamples;
         }
     }
+#else
+    parallelInit();
+    ParallelFor([&](int64_t y) {
+        unsigned short Xi[3] = { 0, 0, y*y*y };
+         for (int x = 0; x < width; x++)
+         {
+            Vec color = Vec();
+            for (int a = 0; a < samples; a++)
+            {
+                Ray ray = m_camera->get_ray(x, y, a > 0, Xi);
+                color = color + m_scene->trace_ray(ray,0,Xi);
+            }
+            m_pixel_buffer[y * width + x] = color * invSamples;
+         }
+    }, height, 32);
+    parallelCleanup();
+#endif
 }
 
 void Renderer::save_image(const char *file_path)
